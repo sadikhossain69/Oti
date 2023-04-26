63c61dd86cbf27f07731c47712087fe4c4f0df84
sadikhossain69 <sadikhossain433@gmail.com>
2023-04-27 01:15:57 +0600

    photo added in dynamic route

--- src/components/pages/projects/ProjectDetails.jsx
+++ src/components/pages/projects/ProjectDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./projectDetails.css"
 import { useParams } from 'react-router-dom';
+import product_banner from "../../../assets/image/product_banner.png"
 
 const ProjectDetails = () => {
 
@@ -13,59 +14,53 @@ const ProjectDetails = () => {
                     <div class="row">
                         <div class="col-lg-5 col-md-5">
                             <div class="product_left">
-                                <img src="image/product_banner.png" alt="" srcset=""/>
+                                <img src={product_banner} alt="" srcset="" />
                             </div>
-                            <div class="left_img d-flex">
-                                <img src="image/product1.png" alt="" srcset=""/>
-                                    <img src="image/product2.png" alt="" srcset=""/>
-                                        <img src="image/product3.png" alt="" srcset=""/>
-                                            <img src="image/product4.png" alt="" srcset=""/>
-                                            </div>
 
-                                        </div>
-                                        <div class="col-lg-6 col-md-7">
-                                            <div class="product_right">
-                                                <h4>Project shopno</h4>
-                                                <h3>Green Valley</h3>
-                                                <div class="d-flex product_Items1">
-                                                    <i class="fa-solid fa-location-dot"></i>
-                                                    <p>Mohammadpur Block D
-                                                        Dahka</p>
-                                                </div>
-                                                <div class="product_item2 d-flex ">
-                                                    <div class="product_2sitem">
-                                                        <i class="fa-solid fa-bed"></i>
-                                                        <span>3 Beds</span>
-                                                    </div>
-                                                    <div class="product_2item">
-                                                        <i class="fa-solid fa-bath"></i>
-                                                        <span>3 Baths</span>
-                                                    </div>
-                                                </div>
-                                                <div class="product_item2  d-flex ">
-                                                    <div class="product_2sitem product_middle">
-                                                        <i class="fa-solid fa-bed"></i>
-                                                        <span>1200sqft</span>
-                                                    </div>
-                                                    <div class="product_2item">
-                                                        <i class="fa-solid fa-car"></i>
-                                                        <span>Car parking</span>
-                                                    </div>
-                                                </div>
-                                                <div class="product_last">
-                                                    <i class="fa-solid fa-compass"></i>
-                                                    <span>South facing</span>
-                                                </div>
-                                                <div class="product_btn">
-                                                    <a href="#">Contact Us</a>
-                                                </div>
-                                            </div>
-                                        </div>
+                        </div>
+                        <div class="col-lg-6 col-md-7">
+                            <div class="product_right">
+                                <h4>Project shopno</h4>
+                                <h3>Green Valley</h3>
+                                <div class="d-flex product_Items1">
+                                    <i class="fa-solid fa-location-dot"></i>
+                                    <p>Mohammadpur Block D
+                                        Dahka</p>
+                                </div>
+                                <div class="product_item2 d-flex ">
+                                    <div class="product_2sitem">
+                                        <i class="fa-solid fa-bed"></i>
+                                        <span>3 Beds</span>
                                     </div>
+                                    <div class="product_2item">
+                                        <i class="fa-solid fa-bath"></i>
+                                        <span>3 Baths</span>
+                                    </div>
+                                </div>
+                                <div class="product_item2  d-flex ">
+                                    <div class="product_2sitem product_middle">
+                                        <i class="fa-solid fa-bed"></i>
+                                        <span>1200sqft</span>
+                                    </div>
+                                    <div class="product_2item">
+                                        <i class="fa-solid fa-car"></i>
+                                        <span>Car parking</span>
+                                    </div>
+                                </div>
+                                <div class="product_last">
+                                    <i class="fa-solid fa-compass"></i>
+                                    <span>South facing</span>
+                                </div>
+                                <div class="product_btn">
+                                    <a href="#">Contact Us</a>
+                                </div>
                             </div>
-                        </section>
-                    </>
-                    );
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
-                    export default ProjectDetails;
+export default ProjectDetails;
