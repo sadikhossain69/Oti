c80b71acab109d2d5e8393e7656f3e0f441c6eaf
sadikhossain69 <sadikhossain433@gmail.com>
2023-05-09 23:36:09 +0600

    massenger add

--- src/App.js
+++ src/App.js
@@ -13,26 +13,31 @@ import 'aos/dist/aos.css'
 import ProjectPage from "./components/pages/projects/ProjectPage";
 import ProjectDetails from "./components/pages/projects/ProjectDetails";
 import ContactPage from "./components/pages/contact/ContactPage";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   useEffect(() => {
     Aos.init({ duration: 2000, offset: 100 });
-  },[])
+  }, [])
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<Aboutpage />} />
-         
-          <Route path="/products" element={<ProjectPage/> } />
-          <Route path="/products/:id" element={<ProjectDetails/> } />
-        
-          <Route path="/contact" element={<ContactPage/>} />
+
+          <Route path="/products" element={<ProjectPage />} />
+          <Route path="/products/:id" element={<ProjectDetails />} />
+
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
+      <MessengerCustomerChat
+        pageId="100091991692359"
+        appId="1371154353446825"
+      />,
     </>
   );
 }
